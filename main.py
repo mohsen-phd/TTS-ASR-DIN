5367939f68c91c95b2db1e8a614e2f2de654d501
Mohsen Fatehifar <mohsen.fatehifar@gmail.com>
2023-02-28 14:21:30 +0000

    use sentence_generator class to feed text to the tts

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 """Main entry point of the program."""
 
+from sentence_generator.generator import Generator
 from tts.tts import GenerateSound
 from tts.utils import play_sound
 
 if __name__ == "__main__":
-    g = GenerateSound(device="cpu")
-    wave = g.get_sound(
-        "Could you provide me with the names of the three capital cities?"
-    )
-    play_sound(wave=wave.squeeze(0), fs=22050)
+    text_generator = Generator()
+    for sentence in text_generator.next_item():
+        g = GenerateSound(device="cpu")
+        wave = g.get_sound(sentence)
+        play_sound(wave=wave.squeeze(0), fs=22050)
