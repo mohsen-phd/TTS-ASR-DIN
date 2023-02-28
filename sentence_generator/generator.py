5d066d3e8dec610120255f98042c1f161d901417
Mohsen Fatehifar <mohsen.fatehifar@gmail.com>
2023-02-28 11:33:14 +0000

    add question generator class

---
+++ sentence_generator/generator.py
@@ -0,0 +1,30 @@
+"""This is a module for generating question string."""
+
+
+class Generator:
+    """This is a class responsible for generating the questions."""
+
+    def __init__(self) -> None:
+        """Initilize the method and create the list of sentences."""
+        self.question_list = [
+            "Could you tell me the names of three different animals?",
+            "Could you provide me with the names of the three capital cities?",
+            "Can you name three sports games?",
+            "What is the opposite of good?",
+            "Can you name three antonyms for the word clean?",
+            "What are some words with the same meaning as bright?",
+            "Is this statement true?  London is a city.",
+            "Do refuse and decline have the same meaning?",
+            "are reindeer and camels both animals?",
+        ]
+
+    def next_item(
+        self,
+    ) -> str:
+        """Iterate over list of questions and return one question at a time.
+
+        Yields:
+            str: one question from the list
+        """
+        for question in self.question_list:
+            yield question
