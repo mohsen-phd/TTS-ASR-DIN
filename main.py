d0be540bfa466eb720f7da4405ac002adee715af
Mohsen Fatehifar <mohsen.fatehifar@gmail.com>
2023-02-28 14:05:26 +0000

    add generate and play sound from text to main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-"""Main entery point of the program."""
+"""Main entry point of the program."""
+
+from tts.tts import GenerateSound
+from tts.utils import play_sound
+
+if __name__ == "__main__":
+    g = GenerateSound(device="cpu")
+    wave = g.get_sound(
+        "Could you provide me with the names of the three capital cities?"
+    )
+    play_sound(wave=wave.squeeze(0), fs=22050)
