c925b5bbd7b7efa1964052418672c97293bb84ad
Mohsen Fatehifar <mohsen.fatehifar@gmail.com>
2023-03-06 10:00:29 +0000

    add asr models

---
+++ asr/asr.py
@@ -0,0 +1,25 @@
+"""Run ASR and convert audio to text."""
+from speechbrain.pretrained import EncoderDecoderASR
+
+
+class ASR:
+    """Use ASR to convert audio to text."""
+
+    def __init__(self) -> None:
+        """Initialize the class by loading HugginFace models."""
+        self.asr_model = EncoderDecoderASR.from_hparams(
+            source="speechbrain/asr-crdnn-rnnlm-librispeech",
+            savedir="models/asr/asr-crdnn-rnnlm-librispeech",
+        )
+
+    def transcribe(self, src: str) -> str:
+        """Get a wav file address and return the transcription of it.
+
+        Args:
+            src (str): Address of the audio file.
+
+        Returns:
+            str: transcribe of the file.
+        """
+        result = self.asr_model.transcribe_file(src)
+        return result
