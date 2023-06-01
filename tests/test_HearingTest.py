1f224a0e4c9475fcf3e54207d6ef9fce5482c05f
Mohsen Fatehifar <mohsen.fatehifar@gmail.com>
2023-06-01 13:57:24 +0100

    add unit test

--- tests/test_HearingTest.py
+++ tests/test_HearingTest.py
@@ -1,13 +1,51 @@
 # flake8: noqa
 import pytest
+from contextlib import nullcontext as does_not_raise
+from hearing_test.test_logic import DigitInNoise, STATUS
 
-from hearing_test.test_logic import DigitInNoise
+
+@pytest.fixture(scope="module")
+def digit_in_noise_test() -> DigitInNoise:
+    return DigitInNoise(
+        correct_threshold=2,
+        incorrect_threshold=1,
+        step_size=[5, 3, 1],
+        reversal_limit=10,
+    )
 
 
 class Test_DigitInNoise:
-    def setup(self):
-        self.obj = DigitInNoise(1, 2, [1, 2], 3)
+    def test_srt(self, digit_in_noise_test: DigitInNoise):
+        digit_in_noise_test._important_snr = [1, 2]
+        assert digit_in_noise_test.srt == 1.5
 
-    def test_srt(self):
-        self.obj._important_snr = [1, 2]
-        assert self.obj.srt == 1.5
+    @pytest.mark.parametrize(
+        "input, expected",
+        [
+            (11, True),
+            (9, False),
+            (10, False),
+            (0, False),
+            (-1, False),
+        ],
+    )
+    def test_stop_condition_valid_input(
+        self, digit_in_noise_test: DigitInNoise, input, expected
+    ):
+        digit_in_noise_test._reversal_count = input
+        assert digit_in_noise_test.stop_condition() == expected
+
+    @pytest.mark.parametrize(
+        "input, expected",
+        [
+            ("11", pytest.raises(TypeError)),
+            (None, pytest.raises(TypeError)),
+            (11, does_not_raise()),
+        ],
+    )
+    def test_stop_condition_invalid_input(
+        self, digit_in_noise_test: DigitInNoise, input, expected
+    ):
+        with expected:
+            digit_in_noise_test._reversal_count = input
+            assert digit_in_noise_test.stop_condition() is not None
