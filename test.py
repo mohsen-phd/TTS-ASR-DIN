8153d7c4fc6fc51080478d6d783d035e0952aada
Mohsen Fatehifar <mohsen.fatehifar@gmail.com>
2023-12-21 13:48:30 +0000

    Delete test file for SimpleASR

--- test.py
+++
@@ -1,8 +0,0 @@
-from get_response.asr import SimpleASR
-import numpy as np
-
-s = SimpleASR()
-w = s.get(
-    "/Users/user/Documents/Projects/PhD/Wordnet-Hearing-Test/train/digit_asr/data/my_test/44khz/my_voice_test_5-6-10.wav"
-)
-print(w)
