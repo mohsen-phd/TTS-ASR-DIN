a33549720709874990508c15df880256e946e6fd
Mohsen Fatehifar <mohsen.fatehifar@gmail.com>
2023-12-29 12:30:28 +0000

    refactor the main.py

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ logger.remove(0)
 logger.add(sys.stderr, level="INFO")
 
 
-def preparation() -> tuple[str, str]:
-    """prepare the test and logging system.
+def preparation() -> tuple[str, str, str]:
+    """Prepare the test and logging system.
 
     Returns:
-        tuple(str,str): save location of logs and audio recording. test number for logging.
+        tuple(str,str,str): save location of logs and audio recording. test number for logging. type of response capturing
     """
     participant_id = input(Fore.GREEN + "Enter The ID: ")
     test_number = input(Fore.GREEN + "Enter test number: ")
+    response_capturing_mode = input(Fore.GREEN + "Enter test mode: ")
 
     save_dir = f"records/{participant_id}"
     os.makedirs(save_dir, exist_ok=True)
@@ -27,15 +28,33 @@ def preparation() -> tuple[str, str]:
     logger.add(f"{save_dir}/{test_number}/out.log")
     logger.debug(f"\nParticipant ID: {participant_id}")
     input(Fore.RED + "Press enter to start the test ")
-    return save_dir, test_number
+    return save_dir, test_number, response_capturing_mode
+
+
+def read_configs(save_dir: str, test_number: str, response_capturing_mode: str) -> dict:
+    """Read config.yaml and update the save location and response capturing mode based on user input.
+
+    Args:
+        save_dir (str): Where to save the logs and audio recording.
+        test_number (str): Which Test is being performed.
+        response_capturing_mode (str): How to capture participant's response.
+
+    Returns:
+        dict: Configurations.
+    """
+    configs = read_conf("config.yaml")
+    configs["test"]["record_save_dir"] = f"{save_dir}/{test_number}"
+    configs["response_capturing"] = response_capturing_mode
+    logger.debug(f"Config file: {configs}")
+    return configs
 
 
 def main():
     """Code entry point."""
-    save_dir, test_number = preparation()
+    save_dir, test_number, response_capturing_mode = preparation()
 
-    configs = read_conf("config.yaml")
-    configs["test"]["record_save_dir"] = f"{save_dir}/{test_number}"
+    configs = read_configs(save_dir, test_number, response_capturing_mode)
+
     manager = get_test_manager(configs)
 
     snr_db = manager.start_snr
